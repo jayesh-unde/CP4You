6776603602ffa8ebbfd4a9d39e2fb8395cbe7b91
Jayesh Unde <jayeshunde2002@gmail.com>
2024-09-05 01:32:44 +0530

    intial commit

---
+++ backend/routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// // authentication routes
+// router.post('/api/send-otp-email',authController.sendOtpEmail);
+// router.post('/api/find-user',authController.findUser);
+// router.post('/api/verify-otp',authController.verifyOtp);
+// router.get('/api/refresh',authController.refresh); // to refresh the access token and refresh token
+// router.post('/api/login-email',authController.loginEmail);
+// router.post('/api/logout',authMiddlewares,authController.logout);
+// router.post('/api/google-login', authController.googleLogin);
+
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5500;
+const router = require('./routes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+
+main().
+then(()=>{
+    console.log("connection successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+}
+const corsOption = {
+    credentials:true,
+    origin:process.env.REACT_APP_API_URL,
+};
+app.use(cors(corsOption));
+app.use(cookieParser());
+
+app.use(express.json({ limit: '8mb' })); // size of file
+app.use(router);
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
